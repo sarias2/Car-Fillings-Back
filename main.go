2364d96f346ce680f884a25f8ceb06b6e5ccb4cf
sarias <sergioariasleon@gmail.com>
2022-08-26 20:43:09 -0500

    Users CRUD
    Full users CRUD working properly.
    TODO:  Create Fillings and Cars CRUD

--- main.go
+++ main.go
@@ -10,10 +10,6 @@ import (
 func main() {
 	app := fiber.New()
 
-	// app.Get("/api", func(c *fiber.Ctx) error {
-	// 	return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB"})
-	// })
-
 	configs.ConnectDB()
 
 	routes.UserRoute(app)
--- routes/user_routes.go
+++ routes/user_routes.go
@@ -1,11 +1,16 @@
 package routes
 
 import (
-	"fillings-back/controllers" //add this
+	"fillings-back/controllers"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func UserRoute(app *fiber.App) {
-	app.Post("/user", controllers.CreateUser) //add this
+
+	app.Post("/user", controllers.CreateUser)
+	app.Get("/user/:userId", controllers.GetAUser)
+	app.Get("/users", controllers.GetAllUsers)
+	app.Put("/user/:userId", controllers.EditAUser)
+	app.Delete("/user/:userId", controllers.DeleteAUser)
 }
